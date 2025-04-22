f7a68878ccdc8dd1e5a2b8cfc7610ffba2a07882
Jiward Torres <jiwardtorres@yahoo.com>
2025-04-22 15:57:19 -0700

    updated

---
+++ BedtimeStrories/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+   // public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+    String input;
+        try {
+            FileInputStream docs = new FileInputStream("goldilocks.txt","hansel and gretel.txt");
+            Scanner scanner = new Scanner(docs);
+
+            while (scanner.hasNextLine());
+            System.out.println("Enter the name of a Story: ");
+            input = scanner.next().toLowerCase();
+
+
+
+
+
+
+
+        }catch (IOException e){
+
+        }
+
+    }
+
+
+
+}
---
+++ Mod01/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        try {
+
+
+
+        }catch (IOException e){
+
+        }
+
+        }
+}
