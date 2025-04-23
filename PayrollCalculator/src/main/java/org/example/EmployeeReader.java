1071c455b3a8263304b05573bc54baee83171c91
Jiward Torres <jiwardtorres@yahoo.com>
2025-04-23 12:21:39 -0700

    Product + Employee

---
+++ PayrollCalculator/src/main/java/org/example/EmployeeReader.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+public class EmployeeReader {
+
+    public static List<Employee> showEmployee(String fileName) {
+        List<Employee> employees = new ArrayList<>();
+        try {
+           BufferedReader buffRead = new BufferedReader(new FileReader(fileName));
+
+            buffRead.readLine();
+            String line;
+            while((line = buffRead.readLine()) != null){
+                String[] token = line.split("|");
+                if(token.length == 4){
+                    int id = Integer.parseInt(token[0]);
+                    String name = token[1];
+                    float hoursW = Float.parseFloat(token[2]);
+                    float rate = Float.parseFloat(token[3]);
+
+                    Employee employ = new Employee(id,name,hoursW,rate);
+
+                    employees.add(employ);
+                }
+            }
+            buffRead.close();
+        } catch (IOException e) {
+            System.out.println("\nError ! Something went wrong.");
+        }
+
+       return employees;
+    }
+}
--- PayrollCalculator/src/main/java/org/example/Main.java
+++ PayrollCalculator/src/main/java/org/example/Main.java
@@ -5,11 +5,21 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Products> product = ProductReader.readProduct("products.csv");
+//        List<Products> product = ProductReader.readProduct("products.csv");
+//
+//        for (Products pro : product){
+//            System.out.println(pro.getName());
+//        }
 
-        for (Products pro : product){
-            System.out.println(pro.getName());
-        }
+    List<Employee> employees = EmployeeReader.showEmployee("employee.csv");
+
+    for(Employee e : employees){
+
+        System.out.printf("Employee ID: %d", e.getEmployeeID());
+        System.out.printf("Employee Name: %s", e.getName());
+       // System.out.println("Employee Gross Pay: %.2f", e.getGrossPay());
+
+    }
 
 
 
