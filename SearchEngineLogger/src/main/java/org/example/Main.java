55b1040ed4d1763bb7fc7d5b2b87dcc7be25dc23
Jiward Torres <jiwardtorres@yahoo.com>
2025-04-24 10:31:45 -0700

    Search Engine Logger

---
+++ SearchEngineLogger/src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        LocalDateTime today = LocalDateTime.now();
+        DateTimeFormatter timeToday = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String getDate = today.format(timeToday);
+        boolean isExited = false;
+        String log = "logs.txt";
+
+        try{
+            FileWriter writer = new FileWriter(log);
+            BufferedWriter buff = new BufferedWriter(writer);
+
+            writer.write(getDate + " launch\n");
+
+            while(!isExited){
+                System.out.print("Enter a search term (X to exit):");
+                String search = scanner.nextLine().toLowerCase();
+                if(search.equals("x")){
+                    writer.write(getDate + " exit");
+                    isExited = true;
+                }else{
+                    writer.write(getDate + " search : " + search + "\n");
+                }
+            }
+        buff.close();
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Wrong input");
+        }
+        catch (IOException e) {
+            System.out.println("ERROR:  An unexpected error occurred");
+            e.getStackTrace();
+        }
+
+
+
+
+
+
+
+    }
+}
