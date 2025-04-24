9454e073cf4f3a15d21acf5af6cea66d2c49a8ad
Jiward Torres <jiwardtorres@yahoo.com>
2025-04-24 11:44:52 -0700

    Search Inventory w name sort

---
+++ SearchInventory/src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String file = "inventory.csv";
+        ArrayList<Product> inventory = getInventory(file);
+
+        System.out.println("We carry the following inventory: ");
+
+        for (int i = 0; i < inventory.size(); i++) {
+            Product p = inventory.get(i);
+            inventory.sort(Comparator.comparing(Product::getName));
+            System.out.printf("id: %d - Name: %s - Price: $%.2f\n",
+                    p.getId(), p.getName(), p.getPrice());
+        }
+    }
+    public static ArrayList<Product> getInventory(String file) {
+        ArrayList<Product> inventory = new ArrayList<>();
+        try{
+            BufferedReader buff = new BufferedReader(new FileReader(file));
+
+            String line;
+            while((line = buff.readLine()) != null){
+                String[] token = line.split("\\|");
+                if(token.length ==3){
+                    int id = Integer.parseInt(token[0]);
+                    String name = token[1];
+                    float price = Float.parseFloat(token[2]);
+
+                    Product pro = new Product(id,name,price);
+                    inventory.add(pro);
+                }
+            }
+            buff.close();
+        }catch (IOException e) {
+            System.out.println("\nError ! File is not found.");
+        }
+        return inventory;
+    }
+
+}
